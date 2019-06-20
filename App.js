f7d038fef663b9cfea62df064c9ee75647fd0670
Thắng Trần Ngọc <ngocthang1493@gmail.com>
2019-06-20 15:36:30 +0700

    add react-native-vector-icons

--- App.js
+++ App.js
@@ -8,7 +8,8 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
+const myIcon = <Icon name="rocket" size={30} color="#900" />;
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
@@ -22,6 +23,7 @@ export default class App extends React.Component {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Icon name="rocket" size={30} color="#900" />
       </View>
     );
   }
